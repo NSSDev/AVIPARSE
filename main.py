2b2754a209f302eb12f465bfe0fe3f3f30458ec7
NSSDev <cdevhashim@mail.ru>
2022-09-23 00:07:01 +0400

    Update

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import openpyxl
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QFileDialog, QAbstractItemView
-import pandas as pd
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 import time
 from tkinter import messagebox
 from selenium.webdriver.common.by import By
-from openpyxl import load_workbook
 import os
-import pyautogui
+from datetime import datetime
+import pytz
+from PIL import Image
+from pytesseract import pytesseract
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import os.path
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -89,7 +95,7 @@ class Ui_MainWindow(object):
         font.setPointSize(11)
         self.pushButton.setFont(font)
         self.pushButton.setStyleSheet("background-color: rgb(0, 170, 0);\n"
-"color: rgb(255, 255, 255);")
+                                      "color: rgb(255, 255, 255);")
         self.pushButton.setObjectName("pushButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
@@ -160,22 +166,25 @@ class Ui_MainWindow(object):
         self.listWidget.setSortingEnabled(__sortingEnabled)
         self.label.setText(_translate("MainWindow", "УСЛУГИ:"))
         self.label_2.setText(_translate("MainWindow", "AVIPARSE"))
-        self.label_3.setText(_translate("MainWindow", "V0.0.1"))
+        self.label_3.setText(_translate("MainWindow", "V0.1.1"))
         self.pushButton.setText(_translate("MainWindow", "Получить данные"))
         self.menu.setTitle(_translate("MainWindow", "Меню"))
         self.action.setText(_translate("MainWindow", "Автор"))
         self.action_2.setText(_translate("MainWindow", "Закрыть"))
 
-
     def author(self):
-        messagebox.showinfo(title="AVIPARSE v0.0.1", message="Разработчик программы:\nTelegram: @HasimNas")
+        messagebox.showinfo(title="AVIPARSE v0.1.1", message="Разработчик программы:\nTelegram: @HasimNas")
 
     def exit_action(self):
         MainWindow.close()
 
     def select_file_directory(self):
-        self.file_path = QFileDialog.getExistingDirectory(None, caption='Выберите папку для сохранения файла',
+        self.file_path = QFileDialog.getExistingDirectory(None, caption='Выберите папку для сохранения картинок',
                                                           directory=os.getcwd())
+
+    def select_json_directory(self):
+        self.json_path = QFileDialog.getExistingDirectory(None, caption='Выберите путь с .json файлом', directory=os.getcwd())
+
     def choose_category(self):
         self.select = self.listWidget.currentRow()
         if self.select == 0:
@@ -194,7 +203,7 @@ class Ui_MainWindow(object):
             self.url = "https://www.avito.ru/rossiya/predlozheniya_uslug/obuchenie_kursy-ASgBAgICAUSYC7afAQ?context=H4sIAAAAAAAA_0q0MrKqLraysFJKK8rPDUhMT1WyLrYyNLNSKipNKspMTizJLwrPTElPLQGLG1gplSQWpaeWwFQaWCkpWdcCAgAA__-OZLTGRgAAAA&p=2"
         if self.select == 7:
             self.url = "https://www.avito.ru/rossiya/predlozheniya_uslug/delovye_uslugi-ASgBAgICAUSYC7KfAQ?context=H4sIAAAAAAAA_0q0MrKqLraysFJKK8rPDUhMT1WyLrYyNLNSKipNKspMTizJLwrPTElPLQGLG1gplSQWpaeWwFQaWCkpWdcCAgAA__-OZLTGRgAAAA&p=2"
-        if self .select == 8:
+        if self.select == 8:
             self.url = "https://www.avito.ru/rossiya/predlozheniya_uslug/ustanovka_tehniki-ASgBAgICAUSYC9r1AQ?context=H4sIAAAAAAAA_0q0MrKqLraysFJKK8rPDUhMT1WyLrYyNLNSKipNKspMTizJLwrPTElPLQGLG1gplSQWpaeWwFQaWCkpWdcCAgAA__-OZLTGRgAAAA&p=2"
         if self.select == 9:
             self.url = "https://www.avito.ru/rossiya/predlozheniya_uslug/master_na_chas-ASgBAgICAUSYC7zvAQ?context=H4sIAAAAAAAA_0q0MrKqLraysFJKK8rPDUhMT1WyLrYyNLNSKipNKspMTizJLwrPTElPLQGLG1gplSQWpaeWwFQaWCkpWdcCAgAA__-OZLTGRgAAAA&p=2"
@@ -211,78 +220,115 @@ class Ui_MainWindow(object):
         if self.select == 15:
             self.url = "https://www.avito.ru/rossiya/predlozheniya_uslug?cd=1&isVertical=1"
 
-
     def collect_info(self):
         for r, d, f in os.walk("C:\\"):
-                for files in f:
-                    if files == "geckodriver.exe":
-                        self.driver_path = os.path.join(r, files)
+            for files in f:
+                if files == "geckodriver.exe":
+                    self.driver_path = os.path.join(r, files)
         options = webdriver.FirefoxOptions()
         options.add_argument("--headless")
         self.driver = webdriver.Firefox(executable_path=self.driver_path, options=options)
         self.driver.maximize_window()
-        counter = 2
-
-        while counter != 100:
+        counter = 0
+        page_counter = 0
+        while counter != 500:
             try:
-                self.driver.get(url=f"{self.url[:-1]}{counter}")
                 counter += 1
+                page_counter += 1
+                self.driver.get(url=f"{self.url[:-1]}{page_counter}")
+                time.sleep(1)
+                if self.url[-9:] == "authsrc=w":
+                    continue
+                if page_counter == 95:
+                    page_counter = 0
                 time.sleep(7)
                 if self.select == 15:
                     items = self.driver.find_elements(By.CLASS_NAME, "styles-list-M1e9B")
                 else:
                     items = self.driver.find_elements(By.CLASS_NAME, "items-items-kAJAg")
+                category_name = self.driver.find_element(By.CLASS_NAME, "page-title-text-tSffu").text
+                # до этого определяем какую подкатегорию выбрал пользователь
                 for item in items:
-                    links = item.find_element(By.TAG_NAME, "a").get_attribute('href')
-                    self.driver.get(links)
-                    time.sleep(2)
-                    id = links[-10:]
-                    self.driver.find_element(By.XPATH,
-                                        "//button[@class='button-button-eBrUW button-button_phone-_Yo3v button-button_card-AkthM button-button-CmK9a button-size-s-r9SeD button-success-_ytiQ']").click()
-                    time.sleep(2)
-                    image = self.driver.find_element(By.XPATH, "//img[@alt='phone']").get_attribute('src')
-                    self.driver.get(image)
-                    if counter == 30:
-                        time.sleep(30)
-                    os.chdir(self.file_path)
-                    if os.path.exists(f"{self.file_path}\\contacts.xlsx"):
-                        fn = "contacts.xlsx"
-                        wb = load_workbook(fn)
-                        ws = wb['ContactsInfo']
-                        ws.append(["ID"])
-                        for i in ws['A']:
-                            if id in i.value:
-                                break
-                        else:
-                            self.driver.save_screenshot(f'{self.file_path}/{id}.png')
-                            ws.append([id])
-                            wb.save(fn)
-                            print(f"[INFO] Добавлено {id} {counter}")
-                    else:
-                        pd = openpyxl.Workbook()
-                        pd_sheet = pd["Sheet"]
-                        pd_sheet.title = "ContactsInfo"
-                        ps = pd.active
-                        ps.append(["ID"])
-                        pd.save(filename=f"{self.file_path}/contacts.xlsx")
+                    links = item.find_elements(By.TAG_NAME, "a")
+                    page_url = [link.get_attribute('href') for link in links]
+                    # получаем каждую ссылку на услугу
+                    for item_url in page_url:  # если в конце ссылки имеется указанный атрибут,то такую ссылку пропускаем
+                        if item_url[-9:] == "authsrc=w":
+                            continue
+                        self.driver.get(url=item_url)
+                        # переходим на ссылку с услугой
+                        id = item_url[-10:]
+                        if self.driver.find_element(By.XPATH, "//div[@data-marker='seller-info/label']").is_displayed():
 
-            except Exception as e:
-                print(e)
+                            status = self.driver.find_element(By.XPATH, "//div[@data-marker='seller-info/label']").text
+                            region = self.driver.find_element(By.XPATH,
+                                                              "//span[@class='style-item-address__string-wt61A']").text
+                            # если там отображено кнопка с телефоном,то продолжаем
+                            self.driver.find_element(By.XPATH,
+                                                     "//button[@class='button-button-eBrUW button-button_phone-_Yo3v button-button_card-AkthM button-button-CmK9a button-size-s-r9SeD button-success-_ytiQ']").click()
+                            time.sleep(2)
+                            image = self.driver.find_element(By.XPATH, "//img[@alt='phone']").get_attribute('src')
+                            self.driver.get(image)
+                            if counter == 30:
+                                time.sleep(30)
+                            if self.driver.current_url[21] == "i":
+                                continue
+                            self.driver.save_screenshot(f"{self.file_path}/{id}.png")
+                            time.sleep(1)
+                            numbers_to_text = Image.open(f"{self.file_path}/{id}.png")
+                            numbers_to_text.save(f"{self.file_path}/{id}.png")
+                            moscow_time = datetime.now(pytz.timezone('Europe/Moscow')).strftime(
+                                "%d/%m/%Y, %H:%M:%S")
+                            path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+                            pytesseract.tesseract_cmd = path_to_tesseract
+                            number = pytesseract.image_to_string(numbers_to_text)
+
+
+                            scope = ["https://spreadsheets.google.com/feeds",
+                                     'https://www.googleapis.com/auth/spreadsheets',
+                                     "https://www.googleapis.com/auth/drive.file",
+                                     "https://www.googleapis.com/auth/drive"]
+                            creds = ServiceAccountCredentials.from_json_keyfile_name(f"{self.json_path}/for_project.json", scope)
+                            spreadsheetId = '1TTB7xi0PgMS1Y0BpKHOgy3jLD7LTZdgJAGn5ojBsseQ'
+                            sheetName = 'ContactsData'
+                            client = gspread.authorize(creds)
+                            sh = client.open_by_key(spreadsheetId)
+                            df2 = pd.DataFrame(
+                                {'col1': [id], 'col2': [region], 'col3': [status], 'col4': [category_name],
+                                 'col5': [number], 'col6': [moscow_time], 'col7':[item_url]})
+                            worksheet = sh.worksheet(sheetName)
+                            values = df2.values.tolist()
+                            x = [item for item in worksheet.col_values(1) if item]
+                            if id in x:
+                                continue
+                            else:
+                                sh.values_append(sheetName, {'valueInputOption': 'USER_ENTERED'}, {'values': values})
+                                continue
+
+
+
+            except NoSuchElementException:
+                pass
 
     def main(self):
-        self.choose_category() #проверка на выбор подкатегории
-        self.select_file_directory() #выбор пути для сохранения результатов
-        if not self.file_path: #если путь не выбран
-            messagebox.showerror(title="AVIPARSE",message="Вы не выбрали папку для сохранения файла !")
+        self.select_file_directory()
+        if not self.file_path:
+            messagebox.showerror(title="AVIPARSE",message="Вы не выбрали папку для сохранения картинок !")
             return
-        else:
-            self.collect_info() #основной метод для парсинга данных
-            messagebox.showinfo(title="AVIPARSE",message="Программа закончила свою работу") #по завершению закрываем selenium
-            self.driver.close()
+        self.select_json_directory()
+        if not self.json_path:
+            messagebox.showerror(title="AVIPARSE",message="Вы не выбрали путь с .json файлом !")
+            return
+        self.choose_category()  # проверка на выбор подкатегории
+        self.collect_info()  # основной метод для парсинга данных
+        messagebox.showinfo(title="AVIPARSE",
+                            message="Программа закончила свою работу")  # по завершению закрываем selenium
+        self.driver.close()
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
